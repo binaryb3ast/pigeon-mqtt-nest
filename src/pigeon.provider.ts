fe19d5d9460b26021bd534b56eb5243b288e1369
Beny <behnammnasehi@gmail.com>
2023-03-19 12:03:08 +0330

    package.json updated use new api aedes

--- src/pigeon.provider.ts
+++ src/pigeon.provider.ts
@@ -1,5 +1,5 @@
 import { Provider, Logger } from "@nestjs/common";
-import { Aedes} from "aedes";
+import * as Aedes from "aedes";
 import { PigeonModuleOptions } from "./pigeon.interface";
 import { INSTANCE_BROKER, PIGEON_OPTION_PROVIDER } from "./pigeon.constant";
 import { createServer } from "aedes-server-factory";
@@ -8,7 +8,9 @@ export function createClientProvider(): Provider {
   return {
     provide: INSTANCE_BROKER,
     useFactory: async (options: PigeonModuleOptions, logger: Logger) => {
-      let broker: Aedes = require("aedes")(options);
+      logger.log("Starting Create Client Provider")
+      Logger.log("Creating Broker Instance")
+      let broker: Aedes = Aedes.createBroker(options);
       await createServer(broker, { ws: true }).listen(options.port);
       return broker;
     },
