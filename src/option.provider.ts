a9c0612bd123adfe24f620815d969d126a9e2341
Beny <behnammnasehi@gmail.com>
2023-03-21 09:07:29 +0330

    commended option.provider.ts

--- src/option.provider.ts
+++ src/option.provider.ts
@@ -2,10 +2,12 @@ import { PigeonModuleAsyncOptions, PigeonModuleOptions, PigeonOptionsFactory } f
 import { Logger, Provider } from '@nestjs/common';
 import { INSTANCE_BROKER, LOGGER_KEY, PIGEON_LOGGER_PROVIDER, PIGEON_OPTION_PROVIDER } from "pigeon.constant";
 
+// Function that creates a NestJS provider for Pigeon MQTT options
 export function createOptionsProvider(
   options: PigeonModuleAsyncOptions,
 ): Provider {
   Logger.log("Creating Option Provider", LOGGER_KEY)
+  // If options include a factory function, create a provider with that factory
   if (options.useFactory) {
     return {
       provide: PIGEON_OPTION_PROVIDER,
@@ -13,7 +15,7 @@ export function createOptionsProvider(
       inject: options.inject || [],
     };
   }
-
+  // If options include an existing provider, create a provider that uses that provider's factory function
   if (options.useExisting) {
     return {
       provide: PIGEON_OPTION_PROVIDER,
@@ -24,13 +26,17 @@ export function createOptionsProvider(
   }
 }
 
+// Function that creates a collection of NestJS providers for Pigeon MQTT options and the MQTT broker instance
 export function createOptionProviders(
   options: PigeonModuleAsyncOptions,
 ): Provider[] {
   Logger.log("Creating Option Provider", LOGGER_KEY)
+  // If options include an existing or factory provider, create a provider array with that provider
   if (options.useExisting || options.useFactory) {
     return [createOptionsProvider(options)];
   }
+
+  // Otherwise, create a provider array with the MQTT broker instance provider and the options provider
   return [
     {
       provide: INSTANCE_BROKER,
@@ -45,8 +51,10 @@ export function createOptionProviders(
   ];
 }
 
+// Function that creates a NestJS provider for the Logger class
 export function createLoggerProvider(options: PigeonModuleOptions | PigeonModuleAsyncOptions): Provider {
   Logger.log("Creating Logger Provider", LOGGER_KEY)
+  // Create a provider for the Logger instance with the name 'MqttModule'
   return {
     provide: PIGEON_LOGGER_PROVIDER,
     useValue: new Logger('MqttModule'),
