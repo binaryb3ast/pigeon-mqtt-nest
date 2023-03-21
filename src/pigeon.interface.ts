cb1fb04a0c13a6eb8727e24e4ff7b3f8a4dab87f
Beny <behnammnasehi@gmail.com>
2023-03-21 09:25:24 +0330

    commended and updatee pigeon.interface.ts

--- src/pigeon.interface.ts
+++ src/pigeon.interface.ts
@@ -2,8 +2,10 @@ import { AedesOptions, PublishPacket } from "aedes";
 import { LoggerService, Type } from "@nestjs/common";
 import { ModuleMetadata } from "@nestjs/common/interfaces";
 
+// Define a type for a function that can transform a message payload
 export type MqttMessageTransformer<T> = (payload: string | Buffer) => T;
 
+// Options for subscribing to MQTT topics
 export interface MqttSubscribeOptions {
   topic: string | string[];
   queue?: boolean;
@@ -11,6 +13,7 @@ export interface MqttSubscribeOptions {
   transform?: "json" | "text" | MqttMessageTransformer<unknown>;
 }
 
+// Custom PublishPacket type with explicit properties
 export interface PubPacket extends PublishPacket {
   cmd: "publish";
   qos: 0 | 1 | 2;
@@ -18,55 +21,56 @@ export interface PubPacket extends PublishPacket {
   payload: string | Buffer;
 }
 
-export class CredentialInterface {
-  username: string;
-  password: Buffer;
-}
-
+// Parameters for a subscriber function
 export interface MqttSubscriberParameter {
   index: number;
   type:
-    | 'error'
-    | 'payload'
-    | 'topic'
-    | 'publish'
-    | 'packet'
-    | 'client'
-    | 'host'
-    | 'subscription'
-    | 'subscriptions'
-    | 'function'
-    | 'credential';
+    | "error"
+    | "payload"
+    | "topic"
+    | "publish"
+    | "packet"
+    | "client"
+    | "host"
+    | "subscription"
+    | "subscriptions"
+    | "function"
+    | "credential"; // The type of parameter being passed to the subscriber function
   transform?: "json" | "text" | MqttMessageTransformer<unknown>;
 }
 
+// Options for subscribing to MQTT topics with a subscriber function
 export interface PigeonSubscriber {
-  topic: RegExp | string;
-  handle: any;
-  provider: any;
-  options: MqttSubscribeOptions;
-  parameters: MqttSubscriberParameter[];
+  topic: RegExp | string; // The topic or regex pattern to subscribe to
+  handle: any; // The subscriber function to call when a message is received
+  provider: any; // The object containing the subscriber function
+  options: MqttSubscribeOptions; // Additional options for the subscriber
+  parameters: MqttSubscriberParameter[]; // The parameters to pass to the subscriber function
 }
 
+// Options for configuring a logger for MQTT messages
 export interface MqttLoggerOptions {
   useValue?: LoggerService;
   useClass?: Type<LoggerService>;
 }
 
+// Options for configuring a Pigeon MQTT module
 export interface PigeonModuleOptions extends AedesOptions {
-  port?: number;
-  portWs?:number;
+  port?: number; // The port to listen on for MQTT connections
+  portWs?: number; // The port to listen on for MQTT over WebSockets connections
 }
 
+// Factory interface for creating Pigeon module options
 export interface PigeonOptionsFactory {
   createPigeonConnectOptions(): Promise<PigeonModuleOptions> | PigeonModuleOptions;
 }
 
 
+// Asynchronous options for configuring a Pigeon MQTT module
 export interface PigeonModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
-  inject?: any[];
-  useExisting?: Type<PigeonOptionsFactory>;
-  useClass?: Type<PigeonOptionsFactory>;
-  useFactory?: (...args: any[]) => Promise<PigeonOptionsFactory> | PigeonOptionsFactory;
-  logger?: MqttLoggerOptions;
+  inject?: any[]; // Additional dependencies to inject
+  useExisting?: Type<PigeonOptionsFactory>; // An existing provider to use for creating options
+  useClass?: Type<PigeonOptionsFactory>; // A class to use for creating options
+  useFactory?: (...args: any[]) => Promise<PigeonOptionsFactory> | PigeonOptionsFactory; // A factory function to use for creating options
+  logger?: MqttLoggerOptions; // Optional logger configuration for MQTT messages
 }
