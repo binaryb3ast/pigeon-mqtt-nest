3398b5a3c813f1c719b34c1f2d905df8ec7da77d
Beny <behnammnasehi@gmail.com>
2023-03-21 09:26:44 +0330

    commended pigeon.module.ts

--- src/pigeon.module.ts
+++ src/pigeon.module.ts
@@ -7,38 +7,39 @@ import { createLoggerProvider, createOptionProviders } from "option.provider";
 import { PigeonExplorer } from "pigeon.explorer";
 import { DiscoveryModule } from "@nestjs/core";
 
-@Global()
+@Global() // Indicates that this module should be available globally
 @Module({
-  imports: [DiscoveryModule],
-  exports: [PigeonService]
+  imports: [DiscoveryModule], // Importing DiscoveryModule to enable automatic service discovery
+  exports: [PigeonService] // Exporting PigeonService to make it available for other modules to use
 })
 export class PigeonModule {
 
+
   public static forRootAsync(options: PigeonModuleAsyncOptions): DynamicModule {
     return {
-      module: PigeonModule,
+      module: PigeonModule, // Current module
       providers: [
-        ...createOptionProviders(options),
-        createLoggerProvider(options),
-        createClientProvider(),
-        PigeonExplorer,
-        PigeonService
+        ...createOptionProviders(options), // Creates providers for Pigeon options
+        createLoggerProvider(options), // Creates a provider for the logger
+        createClientProvider(), // Creates a provider for the MQTT client
+        PigeonExplorer, // Adds the PigeonExplorer service as a provider
+        PigeonService // Adds the PigeonService as a provider
       ]
     };
   }
 
   public static forRoot(options: PigeonModuleOptions): DynamicModule {
     return {
-      module: PigeonModule,
+      module: PigeonModule, // Current module
       providers: [
         {
-          provide: PIGEON_OPTION_PROVIDER,
+          provide: PIGEON_OPTION_PROVIDER, // Provider token for the Pigeon options
           useValue: options
         },
-        createLoggerProvider(options),
-        createClientProvider(),
-        PigeonExplorer,
-        PigeonService
+        createLoggerProvider(options), // Creates a provider for the logger
+        createClientProvider(), // Creates a provider for the MQTT client
+        PigeonExplorer, // Adds the PigeonExplorer service as a provider
+        PigeonService // Adds the PigeonService as a provider
       ]
     };
   }
