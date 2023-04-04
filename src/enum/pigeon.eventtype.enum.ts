12d90b7811e275ed2a30be3cf75e4864c09a23cc
Beny <behnammnasehi@gmail.com>
2023-04-04 16:23:21 +0330

    add SYSTEM_TOPIC = "system_topic"

--- src/enum/pigeon.eventtype.enum.ts
+++ src/enum/pigeon.eventtype.enum.ts
@@ -2,6 +2,7 @@ export enum EventType {
     STRING = "string",
     SEGMENT = "segment",
     REGEXP = "regexp",
+    SYSTEM_TOPIC = "system_topic",
     ARR_STRING = "arr_string",
     ARR_REGEXP = "arr_regexp",
     UNKNOWN = "unknown",
