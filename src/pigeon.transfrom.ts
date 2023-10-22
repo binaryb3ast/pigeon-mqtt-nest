1fc2cc0ada028c535832a6bdca93e22e5a37b1d0
Beny <behnammnasehi@gmail.com>
2023-10-22 11:52:19 +0330

    add documentation

--- src/pigeon.transfrom.ts
+++ src/pigeon.transfrom.ts
@@ -2,26 +2,40 @@ import { MqttMessageTransformer } from 'pigeon.interface';
 type JSONValue = string | number | boolean | JSONObject | JSONArray;
 type JSONArray = Array<JSONValue>;
 
-// Define an interface for JSON objects
+/**
+ * An interface representing a JSON object, where keys are strings and values can be of various types.
+ */
 interface JSONObject {
   [x: string]: JSONValue;
 }
 
-// Define a message transformer that converts JSON string to object
+/**
+ * JSON message transformer function.
+ * @param payload - The message payload as a string or buffer.
+ * @returns Parsed JSON object from the payload.
+ */
 export const JsonTransform: MqttMessageTransformer<JSONObject> = (
   payload?: string | Buffer,
 ) => {
   return JSON.parse(payload.toString('utf-8'));
 };
 
-// Define a message transformer that returns the payload as string
+/**
+ * Get the appropriate message transformer based on the provided value.
+ * @returns The appropriate message transformer function.
+ * @param payload
+ */
 export const TextTransform: MqttMessageTransformer<string> = (
   payload?: string | Buffer,
 ) => {
   return payload.toString('utf-8');
 };
 
-// Get the appropriate message transformer based on the provided value
+/**
+ * Get the appropriate message transformer based on the provided value.
+ * @param transform - The transformation type ('json', 'text', or a custom transformer function).
+ * @returns The appropriate message transformer function.
+ */
 export function getTransform(
   transform?: 'json' | 'text' | MqttMessageTransformer<unknown>,
 ) {
